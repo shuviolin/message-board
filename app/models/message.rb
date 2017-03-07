154437d7bd3e889042f91439fb34251e52566bd4
Shu Sasaki <shusasaki0330@gmail.com>
2017-03-07 03:39:56 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,8 @@ class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
-  validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-
+  validates :body , length: { minimum: 2, maximum: 100 } , presence: true
+#年齢は必須入力かつ０〜１５０の間.必須入力とはしない
+  validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227001611) do
+ActiveRecord::Schema.define(version: 20170307020925) do
 
   create_table "massages", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170227001611) do
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
